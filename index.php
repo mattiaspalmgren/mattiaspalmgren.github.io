6ab38ad5a1346b02a411e8803238da01e0ab1ff3
mattiaspalmgren <mattiaspalmgren92@gmail.com>
2015-11-24 20:31:33 +0100

    init old site

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+include "templates/header.php";
+?>
+
+	<body>
+	
+		<div class="intro">
+
+				<header class="header">
+						<div id="logo"></div>
+						
+				</header>
+
+		</div>
+		
+		<div class="outro">
+				<div class="title-content">
+					<h2> Nyfiken ingenjörsstudent.</h2>
+					<p>22-årig grapp, ursprungligen från de värmländska skogarna. <br> Nu bosatt och studerandes till civilingenjör i medieteknik vid Linköpings universitet, campus Norrköping. <br> Är förtjust i såväl tekniska som estetiska aspekter av den digitala världen. </p> <p>Nedan följer ett axplock av projekt där jag haft fingrarna med i smeten, tillsammans med mina medstudenter.</p>
+					
+					<ul class="links">
+							<li> <a title="twitter.com/palmnish" href="https://twitter.com/palmnish" target="blank"> <i class="fa fa-twitter fa-2x"></i></a> </li>
+							<li> <a title="github.com/mattiaspalmgren" href="https://github.com/mattiaspalmgren" target="blank"> <i class="fa fa-github fa-2x"> </i></a> </li>
+							<li> <a title="se.linkedin.com/in/mattiaspalmgren/" href="https://se.linkedin.com/in/mattiaspalmgren/" target="blank"> <i class="fa fa-linkedin fa-2x"> </i></a> </li>
+							<li><a title="cv_mattiaspalmgren" href="assets/cv_mattiaspalmgren.pdf" target="blank"><i class="fa fa-file-text fa-2x"></i></li></a>
+					</ul>
+
+				</div>
+
+				<div class="container">
+					
+
+					<div class="bubble-content" style="background-image: url('assets/img/cover.png');" ><h2 style="font-family: Exo">POLYGON</h2>
+							
+							<p>	En polygongrafisk rymd i webbläsaren, skapad under parollen “onödigt men härligt”. Gjordes som projekt i kursen 3D datorgrafik. Miljön är skriven i WebGL med hjälp av biblioteket Three.js.<br><a title="https://github.com/petraohlin/Polygon" href="https://github.com/petraohlin/Polygon" target="blank"> <i class="fa fa-github fa-4x"> </i></a></p>
+					</div>
+
+					<div class="bubble-content " style="background-image: url('assets/img/cover2.png');"><h2 style="font-family: Josefin Sans">ATLAS</h2>
+							<p>	Ett geografispel som gjordes som projekt i kursen Kommunikation och användargränssnitt, där en fråga och tillhörande bild ska hinta användaren om vart knappnålen ska placeras. Spelet är skrivet i Javascript, med hjälp av ramverket KinekticJS, med framförallt läsplattor i åtanke som användarenhet.<br> <a title="https://github.com/CarlEnglund/Atlas" href="https://github.com/CarlEnglund/Atlas" target="blank"> <i class="fa fa-github fa-4x"> </i></a></p>
+					</div>
+
+					<div class="bubble-content " style="background-image: url('assets/img/cover3.png');"><h2 style="font-family: 'Rambla', sans-serif;">LiTHEkoll</h2>
+							<p>	Ett webbprojekt i kursen Elektronisk publicering. Idén är att låta användaren föra in och kategorisera sina transaktioner. Tjänsten presenterar sedan datan och ger användaren en överblick över dennes privatekonomi. <br> <a title="https://github.com/klaseskilson/lithekoll" href="https://github.com/klaseskilson/lithekoll" target="blank"> <i class="fa fa-github fa-4x"> </i></a></p>
+					</div>
+
+					<div class="bubble-content " style="background-image: url('assets/img/cover4.png');"><h2 style="font-family: 'Great Vibes', cursive;">Trysch</h2>
+							<p>	Vinnare av hackathonet Hackout for summer i kategorin Bästa koncept. Vi gjorde en prototyp av en webbapp för små kompisevenemang som fotbollsmatch eller pokerkväll. Tanken var att blanda element från Snapchat och Doodle. <br><br> <a title="http://hackoutforsummer.se/" href="http://hackoutforsummer.se/" target="blank">http://hackoutforsummer.se/ </a>
+					</div>
+
+					<div class="bubble-content " style="background-image: url('assets/img/cover5.png');"><h2 style="font-family: 'Londrina Solid', cursive;">LABYRINT</h2>
+							<p>	Projekt i kursen ljudfysik. Idén var att göra en digital variant av det klassiska brädspelet Labyrint, men istället för rattar styra med tonhöjden på rösten.  <br> <a title="github.com/mattiaspalmgren/Labyrint" href="https://github.com/mattiaspalmgren/Labyrint" target="blank"> <i class="fa fa-github fa-4x"> </i></a></p>
+					</div>				
+
+				</div>
+
+		</div>
+
+
+	</body>
+</html>
+
+			
+
---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<footer class="wrapper2">
+				</div>
+
+				<!-- <div class="footer">
+						<h2 class="footer">"Take me to some place where there's music and there's laughter"</h2>
+				</div> -->
+			</foooter>
+
+		
+	</body>
+</html>
